82b777002c90849e4f09b797718696bfedd14fd8
Sujith-Medisetty <70951136+Sujith-Medisetty@users.noreply.github.com>
2025-05-14 14:56:27 -0500

    Update Contact.jsx

--- src/screens/Contact.jsx
+++ src/screens/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
   return (
     <Box id="contact" sx={{ background: `${theme.palette.secondary.main}` }}>
       <ContactSection padding="2rem 8% 3rem">
-        <StyledButton href="https://www.linkedin.com/in/sujith-medisetty-40965a1b4/">
+        <StyledButton href="https://www.linkedin.com/in/sujith-m-40965a1b4/">
           {" "}
           Linked In
         </StyledButton>
