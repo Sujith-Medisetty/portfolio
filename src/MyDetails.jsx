59a04cbc0eef8dbe96759826b25269c1460bb66f
Sujith's Mac <sujithsmac@Sujithss-MacBook-Air.local>
2025-05-15 00:52:05 -0500

    final edits

--- src/MyDetails.jsx
+++ src/MyDetails.jsx
@@ -19,7 +19,7 @@ export const navItems = [
 export const IntroProfessionalDetails = [
   {
     text: "Linked-In",
-    href: "www.linkedin.com/in/sujith-m-185b01237/",
+    href: "https://www.linkedin.com/in/sujith-m-185b01237/",
   },
   {
     text: "Download CV",
